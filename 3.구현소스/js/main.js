d2e04cb1bc96db037b295b4a9da4e933a76b75e1
Tom B. Parker <tombap8@hotmail.com>
2023-09-08 17:33:36 +0900

    탐쌤

---
+++ 3.구현소스/js/main.js
@@ -0,0 +1,11 @@
+// 메인 JS - main.js
+
+const rellax = document.querySelectorAll('.rellax');
+
+rellax.forEach(ele=>{
+    // 패럴렉스적용(릴렉스)
+    new Rellax(ele, {
+        speed: 4
+    }); /////// rellax /////////////
+
+})
